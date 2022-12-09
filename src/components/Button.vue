7e63e3807ed6043fe6eb03040bad2b1927894501
Cemali Gencer <cemali.gencer@gmail.com>
2022-12-09 16:49:39 +0300

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,46 @@
+<template>
+    <button :class="[dark ? 'dark' : 'light', 'baseButton']"> {{ buttonText }}</button>
+</template>
+
+
+<script>
+
+export default ({
+props: {
+    buttonText: {
+        type: String,
+        default: () => 'Button!',
+    },
+    light:{
+        type: Boolean,
+        default: () => true,
+    },
+    dark:{
+        type: Boolean,
+        default: () => false,
+    },
+},
+})
+</script>
+
+
+
+<style scoped>
+.baseButton {
+padding: 10px;
+border:1px solid gray;
+border-radius: 4px;
+cursor: pointer;
+
+}
+
+.light {
+background-color: white;
+color: black;
+}
+.dark {
+background-color: black;
+color: white;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<script>
+import AppButton from '../components/Button.vue'
+
+export default {
+    name: 'Home',
+    components: { 
+        AppButton
+        },
+}
+</script>
+
+<template>
+    <div id="buttons">
+    <AppButton buttonText="Dark" :dark="true" />
+    <AppButton buttonText="Light" />
+    </div>
+
+</template>
+
+<style scoped>
+#buttons {
+    display: flex;
+    flex-direction: column;
+}
+</style>
